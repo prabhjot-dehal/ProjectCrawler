b2cda50b5974513db361b6c5a22adc7a63f32a96
prabhjot-dehal <prabhjotdehal@outlook.com>
2016-11-18 22:31:22 +0000

    trying to add the ground plane in

---
+++ ProjectCrawler/Assets/Scripts/world_floor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class world_floor : MonoBehaviour {
+
+    public int height = 32;
+    public int width = 32;
+
+
+    void Start ()
+    {
+
+        int[,] groundPlane = new int[height, width];
+
+        for (int i = 0; i < width ; i++)
+        {
+
+            for (int x = 0; x < height; i++)
+            {
+                groundPlane[i,x] = new GameObject("mudland_floor{0}{1}",typeof(SpriteRenderer){height}{width});
+            }
+
+        }
+    }
+	
+
+}
