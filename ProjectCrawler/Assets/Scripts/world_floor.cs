030a0d1ad3facde1bc12264d18c88698a88960aa
prabhjot-dehal <prabhjotdehal@outlook.com>
2016-11-19 10:40:59 +0000

    undeleting the mudland floor
    i accidently deleted the all the mudland sprites and this is to undo
    that.

--- ProjectCrawler/Assets/Scripts/world_floor.cs
+++ ProjectCrawler/Assets/Scripts/world_floor.cs
@@ -5,19 +5,23 @@ public class world_floor : MonoBehaviour {
 
     public int height = 32;
     public int width = 32;
+    private string y;
+    Sprite groundSprties = Resources.LoadAll<Sprite>("mudland");
 
 
     void Start ()
     {
 
-        int[,] groundPlane = new int[height, width];
+        GameObject[,] groundPlane = new GameObject[height, width];
 
-        for (int i = 0; i < width ; i++)
+        for (int x = 0; x < width ; x++)
         {
 
-            for (int x = 0; x < height; i++)
+            for (int y = 0; y < height; y++)
             {
-                groundPlane[i,x] = new GameObject("mudland_floor{0}{1}",typeof(SpriteRenderer){height}{width});
+                groundPlane[x, y] = new GameObject(string.Format("tile_{0}_{1}", x, y), typeof(SpriteRenderer));
+                groundPlane[x,y] = GetComponent<SpriteRenderer>().sprite = 
+
             }
 
         }
