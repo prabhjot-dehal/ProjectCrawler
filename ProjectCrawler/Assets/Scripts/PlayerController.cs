677fda6e8532928c6bf469841e2520c05c84b362
prabhjot-dehal <prabhjotdehal@outlook.com>
2016-11-20 16:00:38 +0000

    player controller
    working on the player movement and started the playre animations

---
+++ ProjectCrawler/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    Vector3 move;
+
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+        }
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+        }
+
+    }
+
+
+}
+
+
+//move = new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime, Input.GetAxis("Vertical") * Time.deltaTime, 0);
+//transform.position += move;
+
--- ProjectCrawler/Assets/Scripts/world_floor.cs
+++ ProjectCrawler/Assets/Scripts/world_floor.cs
@@ -1,16 +1,27 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class world_floor : MonoBehaviour {
-
+    Dictionary<string, Sprite> groundSprites;
+    
     public int height = 32;
     public int width = 32;
     private string y;
-    Sprite groundSprties = Resources.LoadAll<Sprite>("mudland");
-
 
     void Start ()
     {
+
+        Sprite[] sprites = Resources.LoadAll<Sprite>("Mudland_floor");
+
+        groundSprites = new Dictionary<string, Sprite>();
+
+        foreach (Sprite sprite in sprites)
+        {
+            Debug.Log(sprite.name);
+
+            groundSprites.Add(sprite.name, sprite);
+        }
+
 
         GameObject[,] groundPlane = new GameObject[height, width];
 
@@ -20,8 +31,9 @@ public class world_floor : MonoBehaviour {
             for (int y = 0; y < height; y++)
             {
                 groundPlane[x, y] = new GameObject(string.Format("tile_{0}_{1}", x, y), typeof(SpriteRenderer));
-                groundPlane[x,y] = GetComponent<SpriteRenderer>().sprite = 
+                groundPlane[x, y].GetComponent<SpriteRenderer>().sprite = groundSprites["Mudland_floor_" + ((8 * (7 - (y % 8))) + (x % 8)).ToString()];
 
+                groundPlane[x,y].transform.position = new Vector2(x, y);
             }
 
         }
